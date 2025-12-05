f05982f66b7993cfb6c0e123e28b6b32c9f81486
Vandeput Jari <vandeputja@gmail.com>
2025-12-05 12:44:34 +0100

    oefenbundel

---
+++ Switch/src/js/main.js
@@ -0,0 +1,10 @@
+import Alpine from 'alpinejs'
+import '../scss/styles.scss'
+
+// Bootstrap + Popper (correcte bundle)
+import * as bootstrap from 'bootstrap'
+
+
+window.Alpine = Alpine
+Alpine.start()
+
